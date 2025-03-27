c390dbb39b23c851dfb927befdac2df86a32bde4
wkhadrao <wkhadrao@LFR-5CG21056J6>
2025-03-27 15:19:39 +0100

    Initial commit: Spring Boot project setup with User API endpoints and Hello controller

---
+++ src/main/java/com/example/Initialized/InitializedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Initialized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Initialized/InitializedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Initialized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
